ac764e41c708a3227bc346726e314d991d817e20
Sarra Barnett <sarrabarnett@gmail.com>
2023-02-08 00:33:49 +0000

    added comments to file

--- hello.py
+++ hello.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3.7
 
-print("Hello World!")
+# This is a full line comment 
+# Comment each line
+# Python doesnt run these comments with # in front
+
+print("Hello World!") # this is a trailing comment 
+
+"""
+This is not a block comment
+This is a multiline string
+"""
